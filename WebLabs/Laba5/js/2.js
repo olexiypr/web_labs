d5053023811a392fab9aeca323209668a1901f20
olexiypr <olexiypr>
2022-10-28 22:00:43 +0300

    Laba5

--- WebLabs/Laba5/js/2.js
+++
@@ -1,6 +0,0 @@
-const area = (len) => {
-    return Math.pow(len,2) * 5 / (4 * Math.tan((180 / 5) * Math.PI/180));
-}
-const len = 10
-const result = area(len)
-document.querySelector(".main-text").innerText += ` AREA: ${result}`
