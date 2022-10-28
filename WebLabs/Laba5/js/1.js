226b74cd4ba6994283455485d58f2254063d6db5
olexiypr <olexiypr>
2022-10-28 22:13:02 +0300

    Revert "Laba5"
    This reverts commit fae982b687333844947f53bd023080da06879269.

--- WebLabs/Laba5/js/1.js
+++
@@ -1,4 +0,0 @@
-const menuText = document.querySelector('.link').innerText
-const footerText = document.querySelector('.footer-text').innerText
-document.querySelector('.link').innerText = footerText;
-document.querySelector('.footer-text').innerText = menuText;
